fcf4b0fe2e1b7916d4044ed588446a3eeefb3d59
Kerby Cruz <kerbyllamosocruz@gmail.com>
2025-10-04 14:22:18 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#322D98',
+                secondary: '#C3CFF6',
+                accent: '#232067',
+            },
+            fontFamily: {
+                montserrat: ['Montserrat', 'sans-serif'],
+                gotu: ['Gotu', 'sans-serif'],
+            }
+        }
+    }
+}
