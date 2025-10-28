58c332e08d6ed45b267b80038934308b3f08e5d1
Acidos-F <anonymousmrsimple@gmail.com>
2025-10-29 00:15:51 +0800

    fixed all broken links

--- profile_page.php
+++ profile_page.php
@@ -50,10 +50,10 @@ if ($result->num_rows > 0) {
         <div class="hidden md:flex items-center space-x-8">
           <a href="index.php"
             class="text-white hover:text-secondary px-3 py-2 rounded-md text-base font-bold">Home</a>
-          <a href="schedule.html"
+          <a href="schedule.php"
             class="text-white hover:text-secondary px-3 py-2 rounded-md text-base font-bold">Schedule</a>
           <a href="faqs.php" class="text-white hover:text-secondary px-3 py-2 rounded-md text-base font-bold">FAQs</a>
-          <a href="contact.html"
+          <a href="contact.php"
             class="text-white hover:text-secondary px-3 py-2 rounded-md text-base font-bold">Contact Us</a>
           
           <?php if (isset($_SESSION['user_id'])):
