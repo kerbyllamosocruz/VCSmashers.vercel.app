3167858c24380a9d78d0090a416d8d7f8d3382d9
kerbyllamosocruz <kerbyllamosocruz@gmail.com>
2026-05-26 16:50:49 +0800

    feat: implement entry point script to handle routing for PHP files

--- api/index.php
+++ api/index.php
@@ -1,17 +1,17 @@
 <?php
-// Set the current working directory to the root folder so relative paths work (like 'config/config.php')
-chdir(__DIR__ . '/../');
-
 $uri = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 
-// Map the root URL to index.php
 if ($uri === '/' || $uri === '') {
     $uri = '/index.php';
 }
 
-$file = __DIR__ . '/..' . $uri;
+$file = realpath(__DIR__ . '/..' . $uri);
 
-if (file_exists($file) && pathinfo($file, PATHINFO_EXTENSION) === 'php') {
+if ($file && file_exists($file) && pathinfo($file, PATHINFO_EXTENSION) === 'php') {
+    // Emulate XAMPP behavior by setting the working directory to the target file's folder
+    chdir(dirname($file));
+    $_SERVER['SCRIPT_FILENAME'] = $file;
+    
     require $file;
 } else {
     http_response_code(404);
